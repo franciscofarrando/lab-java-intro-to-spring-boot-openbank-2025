04f1f2228853e63b178ad2b6aee1910dfa521ba2
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-24 16:51:25 +0200

    run

--- src/main/java/com/example/intro_to_spring_boo/IntroToSpringBooApplication.java
+++ src/main/java/com/example/intro_to_spring_boo/IntroToSpringBooApplication.java
@@ -1,13 +1,14 @@
 package com.example.intro_to_spring_boo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroToSpringBooApplication {
-
+public class IntroToSpringBooApplication
 	public static void main(String[] args) {
+
 		SpringApplication.run(IntroToSpringBooApplication.class, args);
 	}
 
-}
+
