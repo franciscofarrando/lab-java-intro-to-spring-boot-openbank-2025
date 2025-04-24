785279ff4327a3441979f32e668784a299728e03
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-04-24 14:47:10 +0200

    init

---
+++ src/main/java/com/example/intro_to_spring_boo/IntroToSpringBooApplication.java
@@ -0,0 +1,13 @@
+package com.example.intro_to_spring_boo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringBooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringBooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intro_to_spring_boo/IntroToSpringBooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intro_to_spring_boo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToSpringBooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
