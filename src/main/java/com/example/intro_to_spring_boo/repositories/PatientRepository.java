6c6e0c3390d0fd2cfe73dfee75a19e208eec3092
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-05-02 16:20:45 +0200

    final 2

--- src/main/java/com/example/intro_to_spring_boo/repositories/PatientRepository.java
+++ src/main/java/com/example/intro_to_spring_boo/repositories/PatientRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
    // List<Patient> findPatientByBirthRange(String dateOfBirth);
-    List<Patient> findByAdmittedBy_Department(String department);
+    List<Patient> findByAdmittedBy_Department(String department); //EL NOMBRE DESPUES DE FIND, DEBE COINCIDIR CON EL NOMBRE DE LAS COLUMNAS/PARAMETROS DE LA OTRA TABLA/CLASE
     List<Patient> findByAdmittedBy_Status(Status status);
 }
